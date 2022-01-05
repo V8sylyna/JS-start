d8ebf99137aeed13ed35306662569d1fb16fb4fd
V8sylyna <91840249+V8sylyna@users.noreply.github.com>
2022-01-05 17:20:10 +0200

    Add files via upload

---
+++ dz1.2.js
@@ -0,0 +1,11 @@
+const name = "Anna";
+const surName = "Jackson";
+const age = 24; 
+const email = "anna.something@gmail.com";
+const phoneNumber = "123 456 78 90";
+
+let price = 10;
+let productsName = "Soap";
+let productsColor = "green";
+let productsCode = 1284695;
+let status = "Expired"
